8a929f7e51adb0187b4bd6c0f22883c055e97e4e
shakil8838 <eng.shakil8838@gmail.com>
2021-03-16 23:29:24 +0600

    complete commit

--- main.c
+++ main.c
@@ -3,5 +3,55 @@
 int main() {
 
 
-    return 0;
+    // 2 7 4 11 10 12 9 21 24 <- input
+    // 7 9 11 21 2 4 10 12 24 -> output
+
+    int theAry[20];
+    int ary[20];
+    int count = 0;
+    int limit = 0;
+
+    printf("Enter the length of the array(must be less than 20):  ");
+    scanf("%d", &limit);
+
+    for (int i = 0; i < limit; ++i) {
+        printf("Enter your %d value: ", i);
+        scanf("%d", &theAry[i]);
+    }
+
+    for (int i = 0; i < limit; ++i) {
+        for (int j = i + 1; j < limit; ++j) {
+            if (theAry[i] > theAry[j]) {
+                int temp = theAry[i];
+                theAry[i] = theAry[j];
+                theAry[j] = temp;
+            }
+        }
+    }
+
+    for (int i = 0; i < limit; ++i) {
+        if (theAry[i] % 2 == 0) {
+            count++;
+        }
+    }
+
+    int j = 0;
+    int oddLen = limit - count;
+
+    for (int i = 0; i < limit; ++i) {
+        if (theAry[i] % 2 == 1) {
+            if (j < oddLen) {
+                ary[j] = theAry[i];
+                j++;
+            }
+        } else {
+            ary[oddLen] = theAry[i];
+            oddLen++;
+        }
+    }
+
+    for (int i = 0; i < limit; ++i) {
+        printf("%d  ", ary[i]);
+    }
+
 }
