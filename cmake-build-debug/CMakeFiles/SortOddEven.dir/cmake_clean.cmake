641f57231455a717a23cae2afbd4daa9b7675082
shakil8838 <eng.shakil8838@gmail.com>
2021-03-16 20:15:56 +0600

    init commit

---
+++ cmake-build-debug/CMakeFiles/SortOddEven.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SortOddEven.dir/main.c.obj"
+  "SortOddEven.exe"
+  "SortOddEven.exe.manifest"
+  "SortOddEven.pdb"
+  "libSortOddEven.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SortOddEven.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
